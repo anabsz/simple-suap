0b53ec69158f6cd848594470f91446face454cdd
Ana Barbosa <ana.barbosa.dev@gmail.com>
2026-06-22 16:16:55 +0000

    feat(auth): adicionar config e rotas(#16)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { DisciplinaTablePage } from './disciplina/pages/table.page';
 import { DisciplinaCreatePage } from './disciplina/pages/create.page';
 import { DisciplinaEditPage } from './disciplina/pages/update.page';
 import { DisciplinaDetailPage } from './disciplina/pages/detail.page';
+import { LoginPage } from './auth/pages/login.page';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   {
@@ -11,7 +13,12 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
+    path: 'login',
+    component: LoginPage
+  },
+  {
     path: 'disciplinas',
+    canActivate: [authGuard],
     children: [
       {
         path: '',
